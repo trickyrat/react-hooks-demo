467104a327eb742dacc3bee8cc7e63fa06606484
trickyrat <wangjian0513@outlook.com>
2022-11-18 13:34:51 +0800

    feat: add antd layout

--- react-hooks-demo/src/App.tsx
+++ react-hooks-demo/src/App.tsx
@@ -1,55 +1,91 @@
 import './App.css';
-import "bootstrap/dist/css/bootstrap.min.css";
-import "@fortawesome/fontawesome-free/css/all.min.css";
-import "@fortawesome/fontawesome-free/js/all.min.js";
 // import AddBook from "./components/AddBook";
 // import BooksList from "./components/BooksList";
 import { Routes, Route, Link } from "react-router-dom";
-import { AppContainer, ColumnContainer, ColumnTitle, CardContainer } from "./styles";
+import { AppContainer } from "./styles";
 import { Column } from "./components/Column";
 import { AddNewItem } from './components/AddNewItem';
 import { useAppState } from './state/AppStateContext';
 import { addList } from './state/actions';
 import { CustomDragLayer } from './components/CustomDragLayer';
+import { LaptopOutlined, NotificationOutlined, UserOutlined } from '@ant-design/icons';
+import type { MenuProps } from 'antd';
+import { Breadcrumb, Layout, Menu } from 'antd';
+import React from 'react';
+
+const { Header, Content, Sider } = Layout;
+
+const items1: MenuProps['items'] = ['1', '2', '3'].map(key => ({
+  key,
+  label: `nav ${key}`,
+}));
+
+const items2: MenuProps['items'] = [UserOutlined, LaptopOutlined, NotificationOutlined].map(
+  (icon, index) => {
+    const key = String(index + 1);
+
+    return {
+      key: `sub${key}`,
+      icon: React.createElement(icon),
+      label: `subnav ${key}`,
+
+      children: new Array(4).fill(null).map((_, j) => {
+        const subKey = index * 4 + j + 1;
+        return {
+          key: subKey,
+          label: `option${subKey}`,
+        };
+      }),
+    };
+  },
+);
 
 const App: React.FC = () => {
   const { lists, dispatch } = useAppState();
 
   return (
-    <div>
-      {/* <nav className="navbar navbar-expand navbar-dark bg-dark">
-        <a href="/books" className="navbar-brand">
-          react-hook-demo
-        </a>
-        <div className="navbar-nav mr-auto">
-          <li className="nav-item">
-            <Link to={"/books"} className="nav-link">
-              Books
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to={"/add"} className="nav-link">
-              Add
-            </Link>
-          </li>
-        </div>
-      </nav> 
-      <div className="container mt-3">
-        <Routes>
-          <Route path="/" element={<BooksList />} />
-          <Route path="/books" element={<BooksList />} />
-          <Route path="/add" element={<AddBook />} />
-        </Routes>
-      </div> */}
-      <AppContainer>
-        <CustomDragLayer />
-        {lists.map((list) => (
-          <Column text={list.text} key={list.id} id={list.id} />
-        ))}
-        <AddNewItem toggleButtonText="+ Add another list" onAdd={(text) => dispatch(addList(text))} />
-      </AppContainer>
-    </div>
-
+    <Layout>
+      <Header className="header">
+        <div className="logo" />
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']} items={items1} />
+      </Header>
+      <Layout>
+        <Sider width={200} className="site-layout-background">
+          <Menu
+            mode="inline"
+            defaultSelectedKeys={['1']}
+            defaultOpenKeys={['sub1']}
+            style={{ height: '100%', borderRight: 0 }}
+            items={items2}
+          />
+        </Sider>
+        <Layout style={{ padding: '0 24px 24px' }}>
+          <Breadcrumb style={{ margin: '16px 0' }}>
+            <Breadcrumb.Item>Home</Breadcrumb.Item>
+            <Breadcrumb.Item>List</Breadcrumb.Item>
+            <Breadcrumb.Item>App</Breadcrumb.Item>
+          </Breadcrumb>
+          <Content
+            className="site-layout-background"
+            style={{
+              padding: 24,
+              margin: 0,
+              minHeight: 280,
+            }}
+          >
+            <div>
+              <AppContainer>
+                <CustomDragLayer />
+                {lists.map((list) => (
+                  <Column text={list.text} key={list.id} id={list.id} />
+                ))}
+                <AddNewItem toggleButtonText="+ Add another list" onAdd={(text) => dispatch(addList(text))} />
+              </AppContainer>
+            </div>
+          </Content>
+        </Layout>
+      </Layout>
+    </Layout>
   );
 }
 
